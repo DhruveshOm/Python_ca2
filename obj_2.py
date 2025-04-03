d6b1285bd3835d926576d5132cd79d4a9af14b11
Dhruvesh Singh Om <148573305+DhruveshOm@users.noreply.github.com>
2025-04-03 10:27:59 +0530

    Update obj_2.py

--- obj_2.py
+++ obj_2.py
@@ -6,13 +6,12 @@ from scipy import stats
 from sklearn.preprocessing import LabelEncoder
 
 # Load the dataset
-df = pd.read_csv("vgchartz-2024.csv")  # Replace with actual file path
+df = pd.read_csv("vgchartz-2024.csv")  
 
-# Extract a random sample of 10,000 rows
 df_sample = df.sample(n=10000, random_state=42).reset_index(drop=True)
 
+# Implement a loop to categorize games based on their total sales into "Low," "Medium," and "High" sales groups.
 
-# Categorizing games based on total sales (Objective 4)
 def categorize_sales(sales):
     if sales > 5:
         return 'High'
@@ -26,4 +25,3 @@ df_sample['sales_category'] = df_sample['total_sales'].apply(categorize_sales)
 
 
 
-# Implement a loop to categorize games based on their total sales into "Low," "Medium," and "High" sales groups.
